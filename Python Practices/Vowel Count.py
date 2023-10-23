23d397c8b0b68e20e11a3570990a9d2a107aedc5
Victor <vicmorh10@gmail.com>
2023-10-23 09:45:12 -0600

    Creating Count.py (New Practice Program)

---
+++ Python Practices/Vowel Count.py
@@ -0,0 +1,16 @@
+"""
+    Welcome to my Vowel Count.py
+        What this program do is: 
+            you will be requested to type whaterever that you want to and the program will count the vowels that are in that string
+
+    I'm just practicing .count Function
+"""
+
+
+string = input("Type your String: ")
+
+print(f"\nVowel a: {string.count('a')}")
+print(f"\nVowel e: {string.count('e')}")
+print(f"\nVowel i: {string.count('i')}")
+print(f"\nVowel o: {string.count('o')}")
+print(f"\nVowel u: {string.count('u')}")
